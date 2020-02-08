3296a6f541c3de98873b5ed319c9474ca33977d0
Samuel Dale <smhdale@live.com.au>
2020-02-08 15:33:58 +1000

    Fix Mandelbrot GPU generator

--- generators/mandelbrot.gpu.js
+++ generators/mandelbrot.gpu.js
@@ -1,4 +1,4 @@
-const GPU = require('gpu.js')
+const { GPU } = require('gpu.js')
 const converter = require('hsl-to-rgb-for-reals')
 const { rand, randInt } = require('../lib/helpers')
 const { toCartesian } = require('../lib/trig')
@@ -7,47 +7,84 @@ const X_MIN = -3
 const X_MAX = 1
 const Y_MIN = -1.5
 const Y_MAX = 1.5
-const SCALE_MIN = 5
-const SCALE_MAX = 250
-
+const ZOOM_MIN = 50
+const ZOOM_MAX = 1000
 const CIRCLE = 360
-const DEGREES_MIN = CIRCLE / 4
-const DEGREES_MAX = CIRCLE
 
 const RAY_SPEED = 0.01
 const RENDER_SCALE = 0.5
-const FIDELITY = 180
-const BOUND = 4
+const FIDELITY = 1000
 
-function toComplexGenerator(canvas, bounds) {
-	const { x, y, width, height } = bounds
-	const { width: cw, height: ch } = canvas
+function escapeSteps(r, i, fidelity) {
+	const BOUND = 4
 
-	return function toComplex() {
-		return [
-			x + width * (this.thread.x / cw),
-			y + height * (this.thread.y / ch)
-		]
+	let x = r
+	let y = i
+
+	for (let step = 0; step < fidelity; step++) {
+		let xsq = Math.pow(x, 2)
+		let ysq = Math.pow(y, 2)
+
+		// Return early if we have passed the bound
+		if (xsq + ysq > BOUND) return step
+
+		// Otherwise iterate
+		y = 2 * x * y + i
+		x = xsq - ysq + r
+	}
+	
+	return fidelity
+}
+
+function inSet(r, i, fidelity) {
+	return escapeSteps(r, i, fidelity) === fidelity
+}
+
+function initOrigin() {
+	// Pick an origin point in the set
+	let ox = 0
+	let oy = 0
+	do {
+		ox = rand(X_MIN, X_MAX)
+		oy = rand(Y_MIN, Y_MAX)
+	} while (!inSet(ox, oy, FIDELITY))
+
+	// Move along a ray until we escape the set
+	const dir = rand(0, 2 * Math.PI)
+	const delta = toCartesian(RAY_SPEED, dir)
+	do {
+		ox += delta.x
+		oy += delta.y
+	} while (inSet(ox, oy, FIDELITY))
+
+	return [ox, oy]
+}
+
+function initBounds(width, height, origin, zoom) {
+	const [ox, oy] = origin
+	const wHalf = (X_MAX - X_MIN) / (2 * zoom)
+	const hHalf = height / width * wHalf
+	return {
+		x: ox - wHalf,
+		y: oy - hHalf,
+		width: wHalf * 2,
+		height: hHalf * 2
 	}
 }
 
-function getEscapeSteps(r, i) {
-	let step = 0
-	let x = r	// Real component
-	let y = i // Imaginary component
-	while (x ** 2 + y ** 2 < BOUND && ++step < FIDELITY) {
-		let xTmp = x ** 2 - y ** 2 + r
-		y = 2 * x * y * i
-		x = xTmp
-	}
-}
+const easeOutExp = x => x === 1 ? 1 : 1 - Math.pow(2, x * -10)
 
-function inSet(r, i) {
-	return getEscapeSteps(r, i) === FIDELITY
-}
-
-function init() {
-	// Compute origin
+function initPalette(fidelity) {
+	const hueRange = CIRCLE
+	const hueOffset = randInt(0, CIRCLE)
+	return Array
+		.from({ length: fidelity })
+		.map((_, index) => {
+			const hue = hueOffset + easeOutExp(index / fidelity) * hueRange
+			const [r, g, b] = converter(hue % CIRCLE, 1, 0.5)
+			return `rgb(${r},${g},${b})`
+		})
+		.concat(['black'])
 }
 
 module.exports = ctx => {
@@ -60,14 +97,53 @@ module.exports = ctx => {
 	const scaleInv = 1 / RENDER_SCALE
 	ctx.scale(scaleInv, scaleInv)
 
-	// Init GPU renderer
-	const toComplex = toComplexGenerator(canvas, bounds)
-	const gpu = new GPU({ canvas })
-	const render = gpu.createKernel()
-		.addFunction(toComplex)
-		.setGraphical(true)
+	// Choose origin point and zoom scale, then init bounds
+	const origin = initOrigin()
+	const zoom = rand(ZOOM_MIN, ZOOM_MAX)
+	const bounds = initBounds(width, height, origin, zoom)
+	const palette = initPalette(FIDELITY)
+
+	// Init GPU
+	const gpu = new GPU()
+		.addFunction(
+			escapeSteps,
+			{
+				argumentTypes: { r: 'Number', i: 'Number', fidelity: 'Number' },
+				returnType: 'Number'
+			}
+		)
+
+	const compute = gpu
+		.createKernel(function () {
+			// Destructure required values
+			const { x, y } = this.thread
+			const { cw, ch, bx, by, bw, bh, fidelity } = this.constants
+
+			// Convert pixel coord to complex number
+			const r = bx + bw * (x / cw)
+			const i = by + bh * (y / ch)
+
+			// Return escape steps for this coordinate
+			return escapeSteps(r, i, fidelity)
+		})
+		.setConstants({
+			cw: width,
+			ch: height,
+			bx: bounds.x,
+			by: bounds.y,
+			bw: bounds.width,
+			bh: bounds.height,
+			fidelity: FIDELITY
+		})
+		.setLoopMaxIterations(FIDELITY)
 		.setOutput([width, height])
 
-	// Run render
-	render()
+	// Compute on GPU then draw to canvas
+	const result = compute()
+	for (let y = 0; y < height; y++) {
+		for (let x = 0; x < width; x++) {
+			ctx.fillStyle = palette[result[y][x]]
+			ctx.fillRect(x, y, 1, 1)
+		}
+	}
 }
